fefc87722bae5ce8ad09e3815624536a14e09687
Will <programergeek@gmail.com>
2022-06-07 16:58:30 +0200

    [styles] add tiro gurmukhi font

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>Hello World</h1>
+      <h1 className='text-3xl font-tiroGurmukhi underline'>Testing Font</h1>
+      <h2 className='text-2xl font-tiroGurmukhi italic'>This is in italics</h2>
     </div>
   )
 }
