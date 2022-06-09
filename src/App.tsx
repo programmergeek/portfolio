b5e762c0d1ef641f5f5c47f937e91aa9fa029fe6
Will <programergeek@gmail.com>
2022-06-09 14:28:07 +0200

    [styles]change fonts

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <h1 className='text-3xl font-tiroGurmukhi underline'>Testing Font</h1>
-      <h2 className='text-2xl font-tiroGurmukhi italic'>This is in italics</h2>
+      <h1 className='text-3xl font-headers underline'>Fugaz One</h1>
+      <h2 className='text-2xl '>Roboto</h2>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        tiroGurmukhi: ["Tiro Gurmukhi", "serif"]
+        headers: ["Fugaz One", "cursive"]
       }
     },
   },
