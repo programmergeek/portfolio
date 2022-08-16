cd898e54de89e2114ee086d8e7ca584e74b3af89
Will Joseph <programergeek@gmail.com>
2022-08-16 14:47:24 +0200

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function IndexPage() {
+  return (
+    <div>
+      <h1 className="shadow-md flex items-center space-x-4 p-4 m-4">
+        Next.js + TypeScript + Tailwind Css
+      </h1>
+    </div>
+  );
+}
