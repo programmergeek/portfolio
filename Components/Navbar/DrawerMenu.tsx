f3357a8d3b69fc7ff56c1ac1da7825ace364e571
Will Joseph <programergeek@gmail.com>
2022-08-23 12:14:17 +0200

    add logic for drawer toggle

--- Components/Navbar/DrawerMenu.tsx
+++ Components/Navbar/DrawerMenu.tsx
@@ -2,5 +2,20 @@ import React, { useState } from "react";
 import { Box, List, ListItem, ListItemButton } from "@mui/material";
 
 export const DrawerMenu: React.FC = () => {
+  const [isOpen, updateIsOpen] = useState<boolean>(false);
+
+  const toggleDrawer = (open: boolean) => (
+    event: React.KeyboardEvent | React.MouseEvent
+  ) => {
+    if (
+      event.type === "keydown" &&
+      ((event as React.KeyboardEvent).key === "Tab" ||
+        (event as React.KeyboardEvent).key === "Shift")
+    ) {
+      return;
+    }
+    updateIsOpen(open);
+  };
+
   return <Box></Box>;
 };
