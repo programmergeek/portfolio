f046166584737c55e36273d03c5ea1534f086a7a
Will Joseph <programergeek@gmail.com>
2022-08-23 12:26:30 +0200

    add comments

--- Components/Navbar/DrawerMenu.tsx
+++ Components/Navbar/DrawerMenu.tsx
@@ -2,16 +2,20 @@ import React, { useState } from "react";
 import { Box, List, ListItem, ListItemButton } from "@mui/material";
 
 export const DrawerMenu: React.FC = () => {
+  // state to determine if the drawer is open or closed
   const [isOpen, updateIsOpen] = useState<boolean>(false);
 
+  // Accepts a boolean value to toggle the drawer menu.
   const toggleDrawer = (open: boolean) => (
     event: React.KeyboardEvent | React.MouseEvent
   ) => {
     if (
+      // check if the key pressed is 'tab' or 'shift'
       event.type === "keydown" &&
       ((event as React.KeyboardEvent).key === "Tab" ||
         (event as React.KeyboardEvent).key === "Shift")
     ) {
+      // 'if true do nothing'
       return;
     }
     updateIsOpen(open);
