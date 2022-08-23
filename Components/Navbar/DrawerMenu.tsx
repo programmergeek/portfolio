9b58be01d3e964ebb85a313c850025dd758a7605
Will Joseph <programergeek@gmail.com>
2022-08-23 13:39:06 +0200

    add drawer component and drawer button

--- Components/Navbar/DrawerMenu.tsx
+++ Components/Navbar/DrawerMenu.tsx
@@ -1,5 +1,34 @@
 import React, { useState } from "react";
-import { Box, List, ListItem, ListItemButton } from "@mui/material";
+import {
+  Box,
+  Drawer,
+  IconButton,
+  Link,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText
+} from "@mui/material";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+
+const menuItems = [
+  {
+    name: "About",
+    linkTo: "#about"
+  },
+  {
+    name: "Skills",
+    linkTo: "#skills"
+  },
+  {
+    name: "Projects",
+    linkTo: "#projects"
+  },
+  {
+    name: "Contact",
+    linkTo: "#contact"
+  }
+];
 
 export const DrawerMenu: React.FC = () => {
   // state to determine if the drawer is open or closed
@@ -21,5 +50,35 @@ export const DrawerMenu: React.FC = () => {
     updateIsOpen(open);
   };
 
-  return <Box></Box>;
+  // menu shown when drawe is toggled open
+  const menu = () => (
+    <Box
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {menuItems.map((item, index) => (
+          <ListItem key={index} disablePadding>
+            <Link href={item.linkTo} underline="none" color="#000">
+              <ListItemButton>
+                <ListItemText primary={item.name} />
+              </ListItemButton>
+            </Link>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <Box>
+      <IconButton onClick={toggleDrawer(true)}>
+        <MenuRoundedIcon />
+      </IconButton>
+      <Drawer anchor="right" open={isOpen} onClose={toggleDrawer(false)}>
+        {menu()}
+      </Drawer>
+    </Box>
+  );
 };
