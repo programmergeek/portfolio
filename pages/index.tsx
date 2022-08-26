e172cc73f7d92d86d6327bf2468957b59590efb6
Will Joseph <programergeek@gmail.com>
2022-08-26 17:08:30 +0200

    Create forest svg component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import { Navbar } from "../Components/Navbar/Navbar";
+import { Forest } from "../Components/SVG";
 
 export default function IndexPage() {
   return (
     <div>
       <Navbar />
+      <Forest />
     </div>
   );
 }
