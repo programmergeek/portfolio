5e2730aa0ec012b377da6b4413de1302e2df448f
Will Joseph <programergeek@gmail.com>
2022-08-29 11:42:56 +0000

    reorder layers and adjust layer speeds

--- Components/HeroBanner/HeroBanner.tsx
+++ Components/HeroBanner/HeroBanner.tsx
@@ -11,25 +11,23 @@ import { ForestSixthLayer } from "./forestSixthLayer";
 export const HeroBanner = () => {
   return (
     <Box>
-      <Parallax pages={6}>
-        <ParallaxLayer>
+      <Parallax pages={2}>
+        <ParallaxLayer offset={0} speed={0}>
           <ForestSixthLayer />
         </ParallaxLayer>
-        <ParallaxLayer speed={0.6}>
+        <ParallaxLayer offset={0.1} speed={1.5}>
           <ForestFifthLayer />
         </ParallaxLayer>
-        <ParallaxLayer speed={0.7}>
+        <ParallaxLayer offset={0.2} speed={2} >
           <ForestFourthLayer />
         </ParallaxLayer>
-        <ParallaxLayer speed={0.75}>
+        <ParallaxLayer offset={0.25} speed={3}>
           <ForestThirdLayer />
         </ParallaxLayer>
-        <ParallaxLayer
-        speed={0.85}
-        >
+        <ParallaxLayer speed={4} offset={0.3}>
           <ForestSecondLayer />
         </ParallaxLayer>
-        <ParallaxLayer speed={1}>
+        <ParallaxLayer offset={0.32} speed={5}>
           <ForestFirstLayer />
         </ParallaxLayer>
       </Parallax>
